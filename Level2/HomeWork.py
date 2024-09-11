f7a0f3fc7625645c0a823f8a38dc7b7aee0d4773
Nodo <103418655+NoDo-69@users.noreply.github.com>
2024-09-11 20:39:21 +0400

    Level 2 commit

---
+++ Level2/HomeWork.py
@@ -0,0 +1,6 @@
+name = "Nodo"
+age = 16
+favorite_Food = "xinkali"
+favorite_Color = "green"
+
+print(f"hi my name is {name} and iam {age} years old. my favorite food is {favorite_Food}. my favorite color is {favorite_Color}")
