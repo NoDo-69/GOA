364667c60a14c0a58926da60314ae358f7333285
Nodo <103418655+NoDo-69@users.noreply.github.com>
2024-09-15 16:20:05 +0400

    level 3 commit updated

--- Level2/HomeWork.py
+++ Level2/HomeWork.py
@@ -7,9 +7,6 @@ print(f"hi my name is {name} and iam {age} years old. my favorite food is {favor
 
 #level 3 
 
-
-
-
 #my information
 name = "Nodo"
 lastName = "Dolidze"
@@ -19,3 +16,26 @@ countryOfOrigin = "Georgia"
 
 
 print(name,lastName,age,hobby,countryOfOrigin)
+
+
+#math
+number = 1000
+
+#addition
+print(number + 500)
+
+#deduction
+print(number - 500)
+
+#multiplication
+print(number * 2)
+
+#division
+print(number / 2)
+
+#////////
+
+num1 = 500
+num2 = 800
+
+print(num1 + num2)
